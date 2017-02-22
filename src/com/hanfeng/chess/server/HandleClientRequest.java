b20a5d08abbda2e7d4002eebe22ed693c000c323
LifeA <LifeA@DESKTOP-V691LTM>
2017-02-22 18:27:38 +0800

    开学前最后一次提交

--- src/com/hanfeng/chess/server/HandleClientRequest.java
+++ src/com/hanfeng/chess/server/HandleClientRequest.java
@@ -6,6 +6,11 @@ public class HandleClientRequest implements Runnable {
 
 	private InputStream in = null;
 	private OutputStream ot = null;
+	public final int LOGIN = 1;
+	public final int IS_USER_EXIST = 2;
+	public final int REGISTER = 3;
+	public final int GAME = 4;
+	
 	public HandleClientRequest(Socket s) throws IOException {
 		in = s.getInputStream();
 		ot = s.getOutputStream();
@@ -13,10 +18,17 @@ public class HandleClientRequest implements Runnable {
 	
 	public void run() {
 		System.out.println("[INFO]有用户连接");
+		try {
+			phraseClientRequest();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public String phraseClientRequest() {
-		in.read(b)
+	public String phraseClientRequest() throws Exception {
+		byte[] h = new byte[1024];
+		in.read(h);
+		System.out.println(h.toString());
 		return "";
 	}
 	
